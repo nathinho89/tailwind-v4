7bc1e51248d6c6126094e0f5574de5ae89027a3a
nathinho89 <nathinho@outlook.com>
2025-06-24 10:51:36 +0100

    Switched Tailwind to App import

--- app.vue
+++ app.vue
@@ -3,3 +3,7 @@
     <h1 class="font-bold text-3xl text-neutral-500">Hello I am a Tailwind class</h1>
   </div>
 </template>
+
+<style>
+@import "tailwindcss";
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint'],
-  css: ['~/assets/css/main.css'],
   vite: {
     plugins: [
       tailwindcss(),
